83d91ec1fedf033356cb3d47ba7298a82fb6c60a
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-02-02 21:37:14 +0200

    fixed sintax 1 func

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let string = 'The Tao gave birth to machine language.  Machine language gave birth to the assembler. The assembler gave birth to the compiler.  Now there are ten thousand languages. Each language has its purpose, however humble.  Each language expresses the Yin and Yang of software.  Each language has its place within the Tao. But do not program in COBOL if you can avoid it.        -- Geoffrey James, "The Tao of Programming"'
 
 const splitIntoWorld = (anyString) => {  
-    return split = anyString.split(" ");
+    return anyString.split(" ");
 }
 const uniqLetter = (arr) => {
     let map = new Map();
